6f3249aa68163b1e58a3f333db20326d82cbf7b4
Devansh <f20220457@pilani.bits-pilani.ac.in>
2024-08-14 21:33:42 +0530

    finally it worked

--- supplychain-app/Components/Navbar.tsx
+++ supplychain-app/Components/Navbar.tsx
@@ -2,6 +2,7 @@
 import { FC, useEffect,useState } from "react";
 import Web3Modal from 'web3modal';
 import { ethers } from "ethers";
+import {connectWallet} from "./functions"
 type linkelement={
     title:string,
     path:string
@@ -10,24 +11,13 @@ type linkelement={
 export default function Navbar(){
      const [currentUser,setCurrentUser]=useState<string>("");
      const [state,setState]=useState(false);
-     const connectWallet=async()=>{
-        try{
-          const web3Modal=new Web3Modal();
-          const connection=await web3Modal.connect();
-          const provider= new ethers.providers.Web3Provider(connection);
-          const sender=await provider.getSigner().getAddress();
-          setCurrentUser(sender);
-          //send the value of user address into the redux directory
-        }catch(error){
-          console.log(error);
-        }
-      }
      const navigationTypes=[
         {title:"Home",path:"#"},
         {title:"Services",path:"#"},
         {title:"Contact Us",path:"#"},
         {title:"ERC-20",path:"#"}
      ];
+
     //  useEffect(()=>{
     //     document.onclick=(e)=>{
     //         const target=e.target;
@@ -40,7 +30,7 @@ export default function Navbar(){
             <div className="gap-x-14 items-center max-w-screen-xl mx-auto px-4 md:flex md:px-8">
                 <div className="flex items-center justify-between py-5 md:block">
                     <a>
-                       <img src="" width={120} height={50} alt="Logo Image" /> 
+                       <img src="../images/images.png" width={120} height={50} alt="Logo Image" /> 
                     </a>
                     <div className="md:hidden">
                         <button className="menu-btn text-gray-500 hover:text-gray-800">
@@ -64,7 +54,7 @@ export default function Navbar(){
                             </p>
                         ): 
                         (
-                            <button onClick={connectWallet} className="flex items-center justify-center gap-x-1 py-2 px-4 text-white font-medium bg-gray-800 hover:bg-gray-900 rounded-full d:inline-flex ">
+                            <button  onClick={connectWallet} className="flex items-center justify-center gap-x-1 py-2 px-4 text-white font-medium bg-gray-800 hover:bg-gray-900 rounded-full d:inline-flex ">
                                 Connect Wallet
                             </button>
                         )}
